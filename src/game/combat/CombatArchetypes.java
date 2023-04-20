e62e64881d53aeba45d15855e1de12f7d57c9dc0
JeanLKK <jkin0026@student.monash.edu>
2023-04-20 12:50:57 +0800

    Add CombatArchetypes.java

--- src/game/combat/CombatArchetypes.java
+++ src/game/combat/CombatArchetypes.java
@@ -1,4 +1,13 @@
 package game.combat;
 
-public class CombatArchetypes {
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+public abstract class CombatArchetypes {
+    private WeaponItem startingWeapon;
+    private int startingHitRate;
+
+    public CombatArchetypes(WeaponItem startingWeapon, int startingHitRate) {
+        this.startingWeapon = startingWeapon;
+        this.startingHitRate = startingHitRate;
+    }
 }
