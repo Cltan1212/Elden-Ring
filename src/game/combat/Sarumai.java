d2429d1e0668985a7fedad4579ffe3754fd5112b
JeanLKK <jkin0026@student.monash.edu>
2023-04-20 12:54:11 +0800

    Add Sarumai.java

--- src/game/combat/Sarumai.java
+++ src/game/combat/Sarumai.java
@@ -1,4 +1,15 @@
 package game.combat;
 
-public class Sarumai {
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+import game.weapons.Uchigatana;
+
+public class Sarumai extends CombatArchetypes {
+
+    // starting weapon is Uchigatana
+    public Sarumai() {
+        super(new Uchigatana(), 455);
+    }
 }
+
+
+
