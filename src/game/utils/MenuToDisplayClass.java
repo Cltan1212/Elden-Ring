af60b8f371a038a4ffa01c48e8edb9116b6e9d4c
JeanLKK <jkin0026@student.monash.edu>
2023-04-20 13:05:47 +0800

    Add MenuToDisplayClass.java, the purpose is to display the 3 classes/modes for the user to select.

---
+++ src/game/utils/MenuToDisplayClass.java
@@ -0,0 +1,32 @@
+package game.utils;
+
+import game.combat.Bandit;
+import game.combat.Sarumai;
+import game.combat.Wretch;
+
+import java.util.Scanner;
+
+public class MenuToDisplayClass {
+    public char menuToDisplayClass() {
+        Scanner sel = new Scanner(System.in);
+        System.out.println("Select your role: ");
+        System.out.println("b: Bandit");
+        System.out.println("s: Samurai");
+        System.out.println("w: Wretch");
+
+        System.out.println("Select one: ");
+        String choiceString = sel.nextLine();
+        char choiceChar = choiceString.charAt(0);  // convert user input to character
+
+        if (choiceChar == 'b'){
+            new Bandit();
+        } else if (choiceChar == 's'){
+            new Sarumai();
+        } else if (choiceChar == 'w'){
+            new Wretch();
+        }
+
+        return choiceChar;
+    }
+
+}
