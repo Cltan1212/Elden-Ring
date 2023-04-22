781e996ba407f24796a33ebce377b6de1958e721
JeanLKK <jkin0026@student.monash.edu>
2023-04-22 12:32:41 +0800

    Add some code in Application.java for testing/playing the game with another map. (Map is from EdForum A1 REQ1)

--- src/game/Application.java
+++ src/game/Application.java
@@ -7,11 +7,16 @@ import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.FancyGroundFactory;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.World;
+import game.actors.Trader;
 import game.actors.enemies.dog.LoneWolf;
 import game.actors.Player;
 import game.grounds.Dirt;
 import game.grounds.Floor;
+import game.grounds.SiteOfLostGrace;
 import game.grounds.Wall;
+import game.grounds.environments.Graveyard;
+import game.grounds.environments.GustOfWind;
+import game.grounds.environments.PuddleOfWater;
 import game.utils.FancyMessage;
 
 /**
@@ -28,6 +33,7 @@ public class Application {
 		World world = new World(new Display());
 
 		FancyGroundFactory groundFactory = new FancyGroundFactory(new Dirt(), new Wall(), new Floor());
+
 
 		List<String> map = Arrays.asList(
 				"...........................................................................",
@@ -54,6 +60,37 @@ public class Application {
 				"..####__###..................................................._.....__.#...",
 				"..............................................................###..__###...",
 				"...........................................................................");
+
+		// After Enemy class is implemented, can uncomment below to test
+//		FancyGroundFactory groundFactory = new FancyGroundFactory(new Dirt(), new Wall(), new Floor(), new Graveyard(), new GustOfWind(), new PuddleOfWater(), new SiteOfLostGrace());
+//		List<String> map = Arrays.asList(
+//				"..nnnn................................................~~~~~~~~~~~~~~~~~~~~~",
+//				"......................#####....######..................~~~~~~~~~~~~~~~~~~~~",
+//				"..nnnn................#..___....____#...................~~~~~~~~~~~~~~~~~~~",
+//				"..................................__#....................~~~~~~~~~~~~~~~~~~",
+//				"......................._____........#.....................~~~~~~~~~~~~~~~~~",
+//				"......................#............_#......................~~~~~~~~~~~~~~~~",
+//				"......................#...........###......................................",
+//				"...........................................................................",
+//				"...........................................................................",
+//				"~~~~~~~~~~~.......................###___###................................",
+//				"~~~~~~~~~~~~......................________#....nnnn........................",
+//				"~~~~~~~~~~~~~.....................#___U____................................",
+//				"~~~~~~~~~~~~......................#_______#....nnnn........................",
+//				"~~~~~~~~~~~.......................###___###................................",
+//				"~~~~~~~~~~..........................#___#..................................",
+//				"...........................................................................",
+//				"...........................................................................",
+//				"...........................................................................",
+//				"..####__##...........................................&&&......######..##...",
+//				"..#.....__...........................................&&&......#....____....",
+//				"..#___..............&&&..............................&&&........__.....#...",
+//				"..####__###.........&&&......................................._.....__.#...",
+//				"....................&&&.......................................###..__###...",
+//				"...........................................................................");
+//
+
+
 		GameMap gameMap = new GameMap(groundFactory, map);
 		world.addGameMap(gameMap);
 
@@ -72,6 +109,9 @@ public class Application {
 		// HINT: what does it mean to prefer composition to inheritance?
 		Player player = new Player("Tarnished", '@', 300);
 		world.addPlayer(player, gameMap.at(36, 10));
+
+//		Trader trader = new Trader("Trader", 'K', 0);
+//		world.addPlayer(trader, gameMap.at(40, 12));
 
 		world.run();
 	}
