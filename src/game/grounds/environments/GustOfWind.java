f339ef01606919b93c024632c187b8d6d83ddc5b
JeanLKK <jkin0026@student.monash.edu>
2023-04-22 12:05:01 +0800

    Add GustOfWind.java

--- src/game/grounds/environments/GustOfWind.java
+++ src/game/grounds/environments/GustOfWind.java
@@ -1,4 +1,33 @@
 package game.grounds.environments;
 
-public class GustOfWind {
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.enemies.dog.LoneWolf;
+import game.utils.RandomNumberGenerator;
+import game.utils.Status;
+
+public class GustOfWind extends Ground {
+    private static final int SPAWN_CHANCE = 33;
+    public GustOfWind() {
+        super('&');
+    }
+
+    @Override
+    public void tick(Location location){
+        int randomChance = RandomNumberGenerator.getRandomInt(100);
+        if (!location.containsAnActor() && (randomChance <= SPAWN_CHANCE)){
+            location.addActor(new LoneWolf()); // since Graveyard spawn LoneWolf
+        } else{
+            location.setGround(this);
+        }
+    }
+
+    @Override
+    public boolean canActorEnter(Actor actor) {
+        // actor can only enter if hostile to enemy or respawnable
+        return actor.hasCapability(Status.HOSTILE_TO_ENEMY) || actor.hasCapability(Status.RESPAWNABLE);
+    }
+
+
 }
