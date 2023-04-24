001676fd5a8abc284f454a60d52550b6c93168d9
jAcKqQ1 <jwan0405@student.monash.edu>
2023-04-24 10:38:02 +0800

    Complete ConsumeAction class

--- src/game/actions/actorActions/ConsumeAction.java
+++ src/game/actions/actorActions/ConsumeAction.java
@@ -1,4 +1,33 @@
 package game.actions.actorActions;
 
-public class ConsumeAction {
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.items.Consumable;
+import game.items.ConsumableItem;
+import game.utils.FancyMessage;
+import game.utils.Status;
+
+public class ConsumeAction extends Action {
+    private final ConsumableItem item;
+
+    public ConsumeAction(ConsumableItem consumeItem){
+        this.item = consumeItem;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        String returnString = item.consume(actor);
+        return returnString;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " consumes " + item;
+    }
+
+    public String hotkey() {
+        return "f";
+    }
 }
