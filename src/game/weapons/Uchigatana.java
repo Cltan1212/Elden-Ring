9f294edb54f229afe0ee36433c08b99499293f20
jAcKqQ1 <jwan0405@student.monash.edu>
2023-04-25 14:53:23 +0800

    Modify Constructor of the Uchigatana class

--- src/game/weapons/Uchigatana.java
+++ src/game/weapons/Uchigatana.java
@@ -12,15 +12,9 @@ import game.items.Sellable;
 public class Uchigatana extends WeaponItem implements Purchasable, Sellable {
     /**
      * Constructor.
-     *
-     * @param name        name of the item
-     * @param displayChar character to use for display when item is on the ground
-     * @param damage      amount of damage this weapon does
-     * @param verb        verb to use for this weapon, e.g. "hits", "zaps"
-     * @param hitRate     the probability/chance to hit the target.
      */
-    public Uchigatana(String name, char displayChar, int damage, String verb, int hitRate) {
-        super(name, displayChar, damage, verb, hitRate);
+    public Uchigatana (){
+        super("Uchigatana", ')', 115," ", 80);
     }
 
     @Override
