153cc92fad0875adb26e9690547452e582850ef9
jAcKqQ1 <jwan0405@student.monash.edu>
2023-04-25 13:32:33 +0800

    Completed RestAction

--- src/game/actions/actorActions/RestAction.java
+++ src/game/actions/actorActions/RestAction.java
@@ -1,4 +1,28 @@
 package game.actions.actorActions;
 
-public class RestAction {
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.utils.Status;
+
+import java.awt.*;
+
+public class RestAction extends Action {
+
+    private String siteOfLostGrace;
+    public RestAction(String siteOfLostGrace){
+        this.siteOfLostGrace = siteOfLostGrace;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        actor.addCapability(Status.RESTING);
+        new ResetAction().execute(actor, map);
+        return actor + " rested at " + siteOfLostGrace;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " rests at " + siteOfLostGrace;
+    }
 }
