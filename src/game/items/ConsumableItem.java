46380c420117b814dc175b6421f59d968ba71579
jAcKqQ1 <jwan0405@student.monash.edu>
2023-04-25 13:24:40 +0800

    Complete ConsumableItem class with Flask Of Crimson Tears which is a subclass of ConsumableItem

--- src/game/items/ConsumableItem.java
+++ src/game/items/ConsumableItem.java
@@ -1,4 +1,28 @@
 package game.items;
 
-public class ConsumableItem {
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actions.actorActions.ConsumeAction;
+import game.reset.Resettable;
+
+public abstract class ConsumableItem extends Item implements Resettable, Consumable {
+
+    public ConsumableItem(String name, char displayChar, boolean portable){
+        super(name, displayChar, portable);
+    }
+
+
+    @Override
+    public void consume(Actor actor) {
+
+    }
+
+    @Override
+    public String toString(){
+        return super.toString();
+    }
+    @Override
+    public void reset(){
+    }
 }
--- src/game/items/FlaskOfCrimsonTears.java
+++ src/game/items/FlaskOfCrimsonTears.java
@@ -1,4 +1,42 @@
 package game.items;
 
-public class FlaskOfCrimsonTears {
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import game.actions.actorActions.ConsumeAction;
+import game.reset.ResetManager;
+
+public class FlaskOfCrimsonTears extends ConsumableItem{
+    private final int maximumUse = 2;
+    private int remainingConsume;
+    private final int healPoints;
+    public FlaskOfCrimsonTears(){
+        super("Flask Of Crimson Tears", 'c', false);
+        this.remainingConsume = maximumUse;
+        this.healPoints = 250;
+        this.addAction(new ConsumeAction(this));
+        ResetManager.getInstance().registerResettable(this);
+    }
+
+
+    @Override
+    public void consume(Actor actor){
+        String returnString = "";
+        if (remainingConsume > 0){
+            remainingConsume -= 1;
+            actor.heal(healPoints);
+        }
+        else{
+            new Display().println(actor + " has reached maximum consumption.");
+        }
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + " (" + remainingConsume + "/" + maximumUse + ")";
+    }
+
+    @Override
+    public void reset(){
+        this.remainingConsume = maximumUse;
+    }
 }
