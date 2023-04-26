a82fd3f6df437734a97523f64bca7f864f191630
Cltan1212 <110282763+Cltan1212@users.noreply.github.com>
2023-04-26 21:04:17 +0800

    Update Scimitar for requirement 5

--- src/game/weapons/Scimitar.java
+++ src/game/weapons/Scimitar.java
@@ -1,10 +1,24 @@
 package game.weapons;
 
 import edu.monash.fit2099.engine.weapons.WeaponItem;
+import game.actions.runesActions.PurchaseAction;
+import game.actions.runesActions.SellAction;
+import game.items.Purchasable;
+import game.items.Sellable;
 
-public class Scimitar extends WeaponItem {
+public class Scimitar extends WeaponItem implements Purchasable, Sellable {
 
     public Scimitar() {
         super("Scimitar", 's', 118, " ", 88);
+    }
+
+    @Override
+    public PurchaseAction createPurchaseAction() {
+        return new PurchaseAction(this, 600);
+    }
+
+    @Override
+    public SellAction createSellAction() {
+        return new SellAction(this, 100);
     }
 }
