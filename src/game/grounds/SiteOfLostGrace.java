0da972d527ef0795c06ee4e71ed7b4306ba4e38f
jAcKqQ1 <jwan0405@student.monash.edu>
2023-04-26 13:03:15 +0800

    Modified SiteOfLostGrace constructor and added toString method.

--- src/game/grounds/SiteOfLostGrace.java
+++ src/game/grounds/SiteOfLostGrace.java
@@ -4,13 +4,13 @@ import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
+import game.actions.actorActions.RestAction;
 import game.utils.Status;
 
 public class SiteOfLostGrace extends Ground {
 
-    public final String name;
     // allow player to rest on it, when this happens, the entire game will be reset
-
+    private String name;
     public SiteOfLostGrace(String name) {
         super('U');
         this.name = name;
@@ -23,14 +23,18 @@ public class SiteOfLostGrace extends Ground {
 
     // BELOW needs to be add when rest action method is implemented
 
-//    public ActionList allowableActions(Actor actor, Location location, String direction){
-//        ActionList actions = new ActionList();
-//        if (actor.hasCapability(Status.RESTING)){
-////            actions.add(new RestAction(this, ))     WILL NEED TO DEPEND ON RESTACTION METHOD
-//
-//        }
-//        return actions;
-//
-//    }
+    public ActionList allowableActions(Actor actor, Location location, String direction){
+        ActionList actions = new ActionList();
+        if (actor.hasCapability(Status.RESTING)){
+            actions.add(new RestAction(this.toString()));     //WILL NEED TO DEPEND ON RESTACTION METHOD
 
+        }
+        return actions;
+
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
