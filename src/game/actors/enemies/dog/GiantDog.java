542bc980173d0c22527e73b82d566e8bb2a82d90
jAcKqQ1 <jwan0405@student.monash.edu>
2023-04-26 19:48:20 +0800

    Update Range of Dropped Runes for every enemy

--- src/game/actors/enemies/dog/GiantDog.java
+++ src/game/actors/enemies/dog/GiantDog.java
@@ -1,8 +1,15 @@
 package game.actors.enemies.dog;
 
+import game.utils.RandomNumberGenerator;
+
 public class GiantDog extends Dog {
 
     public GiantDog() {
         super("Giant Dog",'G',693);
+    }
+
+    @Override
+    public int generateRunes() {
+        return RandomNumberGenerator.getRandomInt(313,1808);
     }
 }
--- src/game/actors/enemies/water/GiantCrayFish.java
+++ src/game/actors/enemies/water/GiantCrayFish.java
@@ -1,8 +1,14 @@
 package game.actors.enemies.water;
 
+import game.utils.RandomNumberGenerator;
+
 public class GiantCrayFish extends Crustracean{
     public GiantCrayFish() {
         super("Giant Cray Fish", 'R',4803);
     }
 
+    @Override
+    public int generateRunes() {
+        return RandomNumberGenerator.getRandomInt(500,2374);
+    }
 }
