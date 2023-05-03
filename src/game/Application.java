0bdf1cbe9f1a390e11671b3daab78ecbe95e5943
JeanLKK <jkin0026@student.monash.edu>
2023-05-03 20:54:00 +0800

    Add comment for generate javadoc purpose

--- src/game/Application.java
+++ src/game/Application.java
@@ -34,6 +34,11 @@ import game.utils.FancyMessage;
  */
 public class Application {
 
+	/**
+	 * The application of the game engine
+	 * @param args the command line arguments passed to the program
+	 */
+
 	public static void main(String[] args) {
 
 		World world = new World(new Display());
--- src/game/grounds/Wall.java
+++ src/game/grounds/Wall.java
@@ -11,15 +11,30 @@ import edu.monash.fit2099.engine.positions.Ground;
  */
 public class Wall extends Ground {
 
+	/**
+	 * Constructor
+	 * Set the display char for Wwall
+	 */
+
 	public Wall() {
 		super('#');
 	}
+
+	/**
+	 * Method to check if the actor can enter the ground or not
+	 * @param actor the Actor to check
+	 * @return boolean to indicate whether they can enter or not
+	 */
 	
 	@Override
 	public boolean canActorEnter(Actor actor) {
 		return false;
 	}
-	
+
+	/**
+	 * Method to indicate if can block thrown objects or not
+	 * @return boolean to indicate whether it blocks or not
+	 */
 	@Override
 	public boolean blocksThrownObjects() {
 		return true;
