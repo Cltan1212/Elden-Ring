b62564da0093dd4c5c753104290b3ca5ceafcff5
JeanLKK <jkin0026@student.monash.edu>
2023-05-17 12:46:51 +0800

    Create classes for 3 maps (Limgrave, RoundTableHold and StormVeilCastle)

---
+++ src/game/map/MapInitialize.java
@@ -0,0 +1,16 @@
+package game.map;
+
+import java.util.List;
+
+public class MapInitialize {
+
+    List<String> map;
+
+    public MapInitialize(List<String> map) {
+        this.map = map;
+    }
+
+    public List<String> getMap() {
+        return map;
+    }
+}
